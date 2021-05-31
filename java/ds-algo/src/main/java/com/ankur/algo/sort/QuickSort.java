c39678a610fa36b34fb1167d04d743f0f4a62c49
Ankur Sharma <core2411>
2021-05-31 10:25:06 +0530

    Implementation of Quick Select and Quick Sort in Java

---
+++ java/ds-algo/src/main/java/com/ankur/algo/sort/QuickSort.java
@@ -0,0 +1,44 @@
+package com.ankur.algo.sort;
+
+/*
+ * Implementation of Quick Sort
+ */
+public class QuickSort {
+
+	public static void main(String[] args) {
+		int[] arr = {
+				11, 10, 7, 9, 2, 6, 8
+		};
+		quickSort(arr, 0, arr.length);
+		for (int n : arr) {
+			System.out.println(n);
+		}
+	}
+	
+	public static void quickSort(int[] arr, int begin, int end) {
+		if (begin < end) {
+			int pos = partition(arr, begin, end);
+			quickSort(arr, begin, pos);
+			quickSort(arr, pos + 1, end);
+		}
+	}
+	
+	public static int partition(int[] arr, int begin, int end) {
+		int pos = begin;
+		int temp;
+		for (int i = begin; i < end - 1; i++) {
+			if (arr[end - 1] > arr[i]) {
+				if (i != pos) {
+					temp = arr[i];
+					arr[i] = arr[pos];
+					arr[pos] = temp;
+				}
+				pos++;
+			}
+		}
+		temp = arr[end - 1];
+		arr[end - 1] = arr[pos];
+		arr[pos] = temp;
+		return pos;
+	}
+}
