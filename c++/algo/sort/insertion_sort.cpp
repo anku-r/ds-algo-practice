03df35c6421f55ec40c257604d02a6e6c8c1b890
Ankur Sharma <core2411>
2023-03-06 21:11:02 +0530

    Insertion sort array length calculation

--- c++/algo/sort/insertion_sort.cpp
+++ c++/algo/sort/insertion_sort.cpp
@@ -6,8 +6,10 @@ int main()
     //initializing input
     int A[6] = {10, 12, 9, 3, 11, 5};
 
+    int len = sizeof(A) / sizeof(int);
+
     //performing insertion sort
-    for (int i = 1; i < 6; i++)
+    for (int i = 1; i < len; i++)
     {
         int key = A[i];
         int j = i - 1;
